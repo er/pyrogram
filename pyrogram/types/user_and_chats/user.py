701057a88881ddf3c817587bf76f20d99acea442
jordan <45909535+er@users.noreply.github.com>
2024-04-01 00:37:55 +0100

    support NFT usernames

--- pyrogram/types/user_and_chats/user.py
+++ pyrogram/types/user_and_chats/user.py
@@ -169,6 +169,7 @@ class User(Object, Update):
         last_online_date: datetime = None,
         next_offline_date: datetime = None,
         username: str = None,
+        usernames: Optional[List[str]] = None,
         language_code: str = None,
         emoji_status: Optional["types.EmojiStatus"] = None,
         dc_id: int = None,
@@ -196,6 +197,7 @@ class User(Object, Update):
         self.last_online_date = last_online_date
         self.next_offline_date = next_offline_date
         self.username = username
+        self.usernames = usernames
         self.language_code = language_code
         self.emoji_status = emoji_status
         self.dc_id = dc_id
@@ -215,7 +217,10 @@ class User(Object, Update):
     def _parse(client, user: "raw.base.User") -> Optional["User"]:
         if user is None or isinstance(user, raw.types.UserEmpty):
             return None
-
+        if not user.username and len(user.usernames) > 0:
+            username = user.usernames[0].username
+        else:
+            username = user.username
         return User(
             id=user.id,
             is_self=user.is_self,
@@ -232,7 +237,8 @@ class User(Object, Update):
             first_name=user.first_name,
             last_name=user.last_name,
             **User._parse_status(user.status, user.bot),
-            username=user.username,
+            username=username,
+            usernames=[n.username for n in user.usernames],
             language_code=user.lang_code,
             emoji_status=types.EmojiStatus._parse(client, user.emoji_status),
             dc_id=getattr(user.photo, "dc_id", None),
